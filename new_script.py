cce8b3fb7b355e53eca60228c3058f6d7c34a7f6
kosalaMaduwantha <52875765+kosalaMaduwantha@users.noreply.github.com>
2022-08-13 15:10:18 +0530

    Revert "Delete new_script.py"
    This reverts commit fccaa7fbb92e128bef666c761fce77426eebe529.

---
+++ new_script.py
@@ -0,0 +1,24 @@
+# added new changes
+
+print("shit")
+
+
+# -------------
+#kosala - header
+variable_new = 1000
+
+
+
+
+
+
+
+
+
+
+
+
+#------------
+# Kanchal - home
+print("another shit")
+print("math methana wada")
